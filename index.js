a6528d341c9b95c86d0edc32e522fea7e9cf1ede
indraray <techyedutech@gmail.com>
2023-03-05 01:31:37 +0530

    add

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function myFunction(x) {
+function clickMenu() {
    x.classList.toggle("change");
+ }
+
+ //adding the responsive menu bar
+ function myFunction() {
+  var x= document.getElementById("menu_container_id");
+  if(x.className==="menu_container"){
+    x.className += " responsive";
+  }
+  else{
+    x.className = "menu_container";
+  }
  }
