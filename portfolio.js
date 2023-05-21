7d526998e0eb5b56df61fa327539207fd10575f5
indraray <techyedutech@gmail.com>
2023-05-21 20:35:09 +0530

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+function openCard() {
+   var card = document.getElementById("card");
+   card.style.display = "block";
+ }
+
+ function closeCard() {
+   var card = document.getElementById("card");
+   card.style.display = "none";
+ }
