0038a7d409e063c359c1da6c84144481c7658aa9
indraray <techyedutech@gmail.com>
2023-06-24 02:36:13 +0530

    Update Project

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,1 @@
-function openCard() {
-   var card = document.getElementById("card");
-   if(card.style.display == "none"){
-    card.style.display = "block";
-   }
-   else{
-    card.style.display = "none";
-   }
- }
 
- function closeCard() {
-   var card = document.getElementById("card");
-   card.style.display = "none";
- }
